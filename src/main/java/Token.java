6958ca68c124a4ca04107dbfe85171338d33858e
Ahimsaka <41176449+Ahimsaka@users.noreply.github.com>
2020-05-24 14:45:54 -0700

    Delete Token.java
    Shoulda been gitignored.

--- src/main/java/Token.java
+++
@@ -1,8 +0,0 @@
-
-public class Token {
-    public static void main(String[] args) {
-    }
-    public static String token() {
-        return "Njk5Mzk3MTg4OTc1MDY3MTM2.XqJSqg.-tr9SrOUsxmNea6HEeIgF-1lvgs";
-    }
-}
