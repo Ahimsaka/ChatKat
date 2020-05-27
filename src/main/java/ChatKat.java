af19806701ce0f2813dd20a7a35ef231c4126af0
Ahimsaka <devinnarnold@gmail.com>
2020-05-26 17:51:40 -0700

    Abstracted BatchPoints/adding messages to DB, fixed several unpleasant smells.

--- src/main/java/ChatKat.java
+++ src/main/java/ChatKat.java
@@ -16,6 +16,7 @@ import org.influxdb.dto.BatchPoints;
 import org.influxdb.dto.Point;
 import org.influxdb.dto.Query;
 import org.influxdb.dto.QueryResult;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.time.Instant;
@@ -31,34 +32,50 @@ import org.slf4j.LoggerFactory;
 
 public class ChatKat {
     //
-    // create logback logger. the log that logs back, baby!
-    private final static Logger log = LoggerFactory.getLogger(ChatKat.class);
+    // Initialize logger
+    final static Logger log = LoggerFactory.getLogger(ChatKat.class);
     //
-    // initialize variables for Discord Client to facilitate cleanup on exit
-    private static DiscordClient client;
-    //
-    // InfluxDB setup. See: https://github.com/influxdata/influxdb-java/blob/master/MANUAL.md
-    // Note: influxdb-java is used due to ease of configuration with influxDB 1.x, which is used because
-    //       this is a limited scope application and influxDB 2.x does not yet have an official dockerhub image.
-    //       For any further development, conversion to influxdb-client-java and InfluxDB 2.x should be considered.
-    //
-    private static InfluxDB influxDB;
-    private static BatchPoints batchPoints;
-    final static String serverURL = "http://127.0.0.1:8086", username = "root", password = "root";
-    final static String databaseName = "ChatKat";
+    // initialize BatchPoints object to write to database.
+    final static BatchPoints batchPoints = BatchPoints.database(System.getenv("databaseName")).build();
+
+    private static void addMessage(Message message) {
+        //
+        // fetch channelID, authorID as strings to use as and tag values
+        // **numerical strings break the query, so prepend character to each
+        String channelID = "c" + message.getChannelId().asString(),
+                authorID = "a" + message.getAuthorAsMember().block().getId().asString();
+        //
+        // add message data Point to batch
+        batchPoints.point(Point.measurement("messages")
+                .time(message.getTimestamp().toEpochMilli(), TimeUnit.MILLISECONDS)
+                .tag("channelID", channelID)
+                .tag("authorID", authorID)
+                .addField("isValid", 1)
+                .build());
+    }
     //
     public static void main(String[] args) {
-        // do, or do not. there is no
+        /*
+        InfluxDB: See: https://github.com/influxdata/influxdb-java/blob/master/MANUAL.md
+        Note: influxdb-java is used due to ease of configuration with influxDB 1.x, which is used because
+              this is a limited scope application and influxDB 2.x does not yet have an official dockerhub image.
+              For any further development, conversion to influxdb-client-java and InfluxDB 2.x should be considered.
+        */
+        // Build influxDB client.
+        final InfluxDB influxDB = InfluxDBFactory.connect(System.getenv("serverURL"),
+                System.getenv("databaseUser"),
+                System.getenv("databasePass"));
+        //
+        // Build DiscordClient
+        final DiscordClient client = DiscordClientBuilder.create(System.getenv("TOKEN")).build();
+        //
         try {
             //
-            // Build InfluxDB client
-            influxDB = InfluxDBFactory.connect(serverURL, username, password);
-            //
-            // create Database (this will not overwrite existing database on restart)
-            influxDB.query(new Query("CREATE DATABASE " + databaseName));
+            // create Database (influxDB create query is idempotent)
+            influxDB.query(new Query("CREATE DATABASE " + System.getenv("databaseName")));
             //
             // set the default database to write message points to
-            influxDB.setDatabase(databaseName);
+            influxDB.setDatabase(System.getenv("databaseName"));
             //
             // enable writing data in batches & set up exception handler to log failed data points
             influxDB.enableBatch(BatchOptions.DEFAULTS.exceptionHandler(
@@ -66,12 +83,6 @@ public class ChatKat {
                         log.error(p.toString());
                     }))
             );
-            //
-            // initialize BatchPoints object to aggregate and reduce http api calls
-            batchPoints = BatchPoints.database(databaseName).build();
-            //
-            // Build DiscordClient
-            client = DiscordClientBuilder.create(System.getenv("TOKEN")).build();
             //
             // Get an event dispatcher for readyEvents emitted when the bot logs in
             client.getEventDispatcher().on(ReadyEvent.class)
@@ -81,30 +92,12 @@ public class ChatKat {
             // Get guildCreateEvent dispatcher to back-fill DB
             client.getEventDispatcher().on(GuildCreateEvent.class)
                     .map(GuildCreateEvent::getGuild)
-                    .map(guild -> guild.getChannels()
-                            .filter(guildChannel -> guildChannel instanceof TextChannel)
-                            .map(guildChannel -> {
-                                TextChannel textChannel = (TextChannel) guildChannel;
-                                return textChannel.getMessagesBefore(Snowflake.of(Instant.now()))
-                                        .filter(message -> !message.getAuthor().get().isBot() && message.getContent().isPresent())
-                                        .map(message -> {
-                                            //
-                                            // fetch guildID, channelID, authorID as strings to use as measurement and tag values
-                                            // numerical strings break the query, so prepend character to each
-                                            String channelID = "c" + message.getChannelId().asString(),
-                                                    authorID = "a" + message.getAuthorAsMember().block().getId().asString();
-                                            //
-                                            // add to write batch
-                                            batchPoints.point(Point.measurement("messages")
-                                                    .time(message.getTimestamp().toEpochMilli(), TimeUnit.MILLISECONDS)
-                                                    .tag("channelID", channelID)
-                                                    .tag("authorID", authorID)
-                                                    .addField("isValid", 1)
-                                                    .build());
-                                            return message;
-                                        })
-                                        .collectList().block();
-                            }).collectList().block()).subscribe(list -> log.info("First back-fill event emitted"));
+                    .flatMap(Guild::getChannels)
+                    .filter(guildChannel -> guildChannel instanceof TextChannel)
+                    .map(guildChannel -> (TextChannel) guildChannel)
+                    .flatMap(textChannel -> textChannel.getMessagesBefore(Snowflake.of(Instant.now())))
+                    .filter(message -> !message.getAuthor().get().isBot() && message.getContent().isPresent())
+                    .subscribe(message -> addMessage(message));
             //
             // Process incoming messages
             // Note - in event of timing conflict with the back-fill process, the batchPoints
@@ -113,22 +106,9 @@ public class ChatKat {
                     .map(MessageCreateEvent::getMessage)
                     //
                     // we only care about messages with content and non-bot authors.
-                    .filter(message -> !message.getAuthor().get().isBot()
-                            && message.getContent().isPresent())
+                    .filter(message -> !message.getAuthor().get().isBot() && message.getContent().isPresent())
                     .map(message -> {
-                        //
-                        // fetch channelID, authorID as strings to use as and tag values
-                        // **numerical strings break the query, so prepend character to each
-                        String channelID = "c" + message.getChannelId().asString(),
-                                authorID = "a" + message.getAuthorAsMember().block().getId().asString();
-                        //
-                        // add message data Point to batch
-                        batchPoints.point(Point.measurement("messages")
-                                .time(message.getTimestamp().toEpochMilli(), TimeUnit.MILLISECONDS)
-                                .tag("channelID", channelID)
-                                .tag("authorID", authorID)
-                                .addField("isValid", 1)
-                                .build());
+                        addMessage(message);
                         return message;
                     })
                     //
@@ -138,23 +118,16 @@ public class ChatKat {
                     // if the message is a request:
                     .map(message -> {
                         //
-                        // get params, if any
-                        String request = message.getContent().get();
-                        //
-                        // get guildId to get author nickname later
-                        Snowflake guildID = message.getGuild().block().getId();
+                        // write batchPoints to clear cache - ensures that all messages up to request (and possibly
+                        // just after) will be included in output
+                        influxDB.write(batchPoints);
                         //
                         // fetch channel and display typing status
                         MessageChannel channel = message.getChannel().block();
                         channel.type();
                         //
-                        // fetch channelID as strings to use in query
-                        // numerical strings break the query, so prepend character to each
-                        String channelID = "c" + message.getChannelId().asString();
-                        //
-                        // write batchPoints to clear cache - ensures that all messages up to request (and possibly
-                        // just after) will be included in output
-                        influxDB.write(batchPoints);
+                        // get params, if any
+                        String request = message.getContent().get();
                         //
                         // create HashMap for parsing time parameters
                         HashMap<String, Instant> setInterval = new HashMap<>(4);
@@ -174,6 +147,10 @@ public class ChatKat {
                             if (setInterval.containsKey(s.toLowerCase())) interval = setInterval.get(s.toLowerCase()).toEpochMilli();
                         }
                         //
+                        // fetch channelID as strings to use in query
+                        // numerical strings break the query, so prepend character to each
+                        String channelID = "c" + message.getChannelId().asString();
+                        //
                         // Query database
                         QueryResult queryResult = influxDB.query(new Query("SELECT sum(\"isValid\") FROM messages"
                                 + " WHERE channelID = '" + channelID + "'"
@@ -181,26 +158,24 @@ public class ChatKat {
                                 + " GROUP BY authorID"
                         ));
                         //
+                        // get guildId to get author's guild nickname
+                        Snowflake guildID = message.getGuild().block().getId();
+                        //
                         // stream the query results to sort by value and convert to list of strings.
                         // then set them to a list for output
-                        List<String> outputList = queryResult.getResults().get(0).getSeries()
-                                .stream()
-                                .sorted((series1, series2)-> series2.getValues().get(0).get(1).toString().compareTo(series1.getValues().get(0).get(1).toString()))
-                                //
-                                // construct output string for each user and collect to list
-                                .map(series -> client.getMemberById(guildID,
-                                        Snowflake.of(series.getTags().get("authorID").substring(1))).block().getMention()
-                                        + " sent **" + series.getValues().get(0).get(1).toString().split("\\.")[0] + "** messages.")
-                                .collect(Collectors.toList());
-                        //
-                        // prepend number to each entry in list for presentation
-                        outputList.forEach((entry)->{
-                            int i = outputList.indexOf(entry);
-                            outputList.set(i, (i+1) + ". " + entry);
-                        });
+                        List<QueryResult.Series> seriesList = queryResult.getResults().get(0).getSeries();
+                        StringBuilder output = new StringBuilder();
+
+                        seriesList.sort((series1, series2)-> series2.getValues().get(0).get(1).toString()
+                                        .compareTo(series1.getValues().get(0).get(1).toString()));
+
+                        for (int i = 0; i < seriesList.size(); i++) {
+                            output.append(i + 1).append(". ").append(client.getMemberById(guildID,
+                                    Snowflake.of(seriesList.get(i).getTags().get("authorID").substring(1))).block().getMention()).append(" sent **").append(seriesList.get(i).getValues().get(0).get(1).toString().split("\\.")[0]).append("** messages. \n");
+                        }
                         //
                         // join list with \n delimiter to send as one message, then send.
-                        channel.createMessage(String.join("\n", outputList)).subscribe();
+                        channel.createMessage(output.toString()).subscribe();
                         return Mono.empty();
                     })
                     .onErrorContinue((t, o) -> log.error("Error while processing event", t))
@@ -216,10 +191,7 @@ public class ChatKat {
                         // get channelID
                         String channelID = "c" + event.getChannelId().asString();
                         //
-                        // Deleting individual points isn't supported. so we will overwrite the point for the deleted
-                        // message, changing the "isValid" field to 0 for deleted messages. To overwrite points, we need
-                        // to have the full tag set, but Discord does not serve authorID with MessageDeleteEvents.
-                        // This ugly block of code retrieves the authorID from the original record.
+                        // Lazy Delete
                         String authorID = influxDB.query(new Query("SELECT \"isValid\", \"authorID\" FROM messages"
                                 + " WHERE channelID = '" + channelID + "'"
                                 + " AND time = " + messageTime + "ms")).getResults().get(0).getSeries().get(0).getValues().get(0).get(2).toString();
